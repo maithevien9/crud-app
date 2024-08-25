49f1c06fd35f50b9fbdbda5d049201455c73d593
vien.maithe <vien.maithe@enouvo.com>
2024-08-25 15:19:08 +0700

    feat: add auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FileUploadModule } from './file/file.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import databaseConfig from './config/database.config';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { SessionModule } from './session/session.module';
+import { MailerModule } from './mailer/mailer.module';
+import { MailsModule } from './mails/mails.module';
+import { ForgotPasswordModule } from './forgot-password/forgot-password.module';
+import { AuthGoogleModule } from './auth-google/auth-google.module';
+import appConfig from './config/app.config';
+import authConfig from './config/auth.config';
+import mailerConfig from './config/mailer.config';
+import googleConfig from './config/google.config';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, appConfig, authConfig, mailerConfig, googleConfig],
+      envFilePath: ['.env'],
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST || 'localhost',
-      port: parseInt(process.env.MYSQL_PORT) || 3306,
-      username: process.env.MYSQL_USER || 'root',
-      password: process.env.MYSQL_PASSWORD || 'rootpassword',
-      database: process.env.MYSQL_DATABASE || 'testdb',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        const dataSource = await new DataSource(options).initialize();
+        return dataSource;
+      },
     }),
-    // TaskModule,
-    FileUploadModule,
+    UsersModule,
+    AuthModule,
+    SessionModule,
+    MailerModule,
+    MailsModule,
+    ForgotPasswordModule,
+    AuthGoogleModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
